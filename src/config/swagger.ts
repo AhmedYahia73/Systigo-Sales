dbef88dec8b7a9e215556a114a9d2647f2b47f28
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-18 15:26:27 +0300

    fdgrt

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -14,7 +14,7 @@ const options: swaggerJSDoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:3000", 
+        url: "https://sales.systego.net", 
         description: "Development Server",
       },
     ],
@@ -42,5 +42,5 @@ const swaggerSpec = swaggerJSDoc(options);
 export const setupSwagger = (app: Express) => {
   // رابط عرض واجهة Swagger
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("📝 Swagger documentation is available at http://localhost:3000/api-docs");
+  console.log("📝 Swagger documentation is available at https://sales.systego.net/api-docs");
 };
