3928e1f8f7d06e173ff152d20615b7773c618d88
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-22 16:29:33 +0300

    commit

--- src/controllers/admin/target.ts
+++ src/controllers/admin/target.ts
@@ -92,10 +92,7 @@ export const getAllTargets = async (req: Request, res: Response) => {
 
 // ✅ Get Target By ID (معدلة لجلب التارجت مع الـ items والـ sales المعنيين)
 export const lists = async (req: Request, res: Response) => {
-  const validated = await targetIdSchema.parseAsync({ params: req.params });
-  const { id } = validated.params; 
 
-  // 1. جلب التارجت الرئيسي
   const sales = await db
     .select({
       id: users.id,
